40e5035af6fb45bf8252577a0e73a9974db78389
Matteo Meli <matteo.meli@gmail.com>
2015-11-18 14:18:57 +0000

    Reorganize project in i386 and x86_64 architectures

---
+++ src/arch/x86_64/multiboot_header.s
@@ -0,0 +1,15 @@
+section .multiboot_header
+header_start:
+    dd 0xe85250d6                   ; magic number (multiboot 2)
+    dd 0x0                          ; multiboot flag for architecture 0 (protected mode i386)
+    dd header_end - header_start    ; header length
+    ; calculate the checksum (magic number + checksum + flags + header length should equal 0)
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))                                
+    
+    ; insert optional multiboot tags
+
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
+header_end:
