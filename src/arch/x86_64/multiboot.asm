702d5b3cb3db7f73ff6cefea158366f362885e20
Matteo Meli <matteo.meli@gmail.com>
2015-12-14 13:24:18 +0000

    Refactor multiboot header code

--- src/arch/x86_64/multiboot.asm
+++ src/arch/x86_64/multiboot.asm
@@ -1,15 +1,15 @@
-
-MULTIBOOT_MAGIC     equ 0xe85250d6
-ARCHITECTURE_FLAG   equ 0
+MB_MAGIC        equ 0xe85250d6
+MB_ARCH_FLAG    equ 0
+MB_HEADER_LEN   equ mb2_header_end - mb2_header_start
 
 section .multiboot2
 align 8
-header_start:
-    dd MULTIBOOT_MAGIC              ; magic number (multiboot 2)
-    dd ARCHITECTURE_FLAG            ; multiboot flag for architecture 0 (protected mode i386)
-    dd header_end - header_start    ; header length
+mb2_header_start:
+    dd MB_MAGIC             ; magic number (multiboot 2)
+    dd MB_ARCH_FLAG         ; multiboot flag for architecture 0 (protected mode i386)
+    dd MB_HEADER_LEN        ; header length
     ; calculate the checksum (magic number + checksum + flags + header length should equal 0)
-    dd 0x100000000 - (MULTIBOOT_MAGIC + ARCHITECTURE_FLAG + (header_end - header_start))                                
+    dd 0x100000000 - (MB_MAGIC + MB_ARCH_FLAG + MB_HEADER_LEN)                                
     
     ; insert optional multiboot tags
 
@@ -17,4 +17,4 @@ header_start:
     dw 0    ; type
     dw 0    ; flags
     dd 8    ; size
-header_end:
+mb2_header_end:
