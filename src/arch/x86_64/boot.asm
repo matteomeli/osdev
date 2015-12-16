08ce2a505bfd33a3cecd5ceac8b984a836623a8e
Matteo Meli <matteo.meli@gmail.com>
2015-12-16 13:29:35 +0000

    SSE setup is done in long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -16,7 +16,6 @@ start:
 
     call setup_page_tables
     call enable_paging
-    call setup_SSE
 
     ; load the 64bit GDT
     lgdt [gdt64.pointer]
@@ -138,28 +137,6 @@ enable_paging:
     mov cr0, eax
 
     ret
-
-; Check for SSE and enable it. If it's not supported throw error "a".
-setup_SSE:
-    ; check for SSE
-    mov eax, 0x1
-    cpuid
-    test edx, 1<<25
-    jz .no_SSE
-
-    ; enable SSE
-    mov eax, cr0
-    and ax, 0xFFFB      ; clear coprocessor emulation CR0.EM
-    or ax, 0x2          ; set coprocessor monitoring  CR0.MP
-    mov cr0, eax
-    mov eax, cr4
-    or ax, 3 << 9       ; set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
-    mov cr4, eax
-
-    ret
-.no_SSE:
-    mov al, "a"
-    jmp error
 
 ; Prints `ERR: ` and the given error code to screen and hangs.
 ; parameter: error code (in ascii) in al
